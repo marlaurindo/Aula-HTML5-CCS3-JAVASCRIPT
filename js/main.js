7aff6430ac687eb1112798537145356bd1382358
marlaurindo <marlaurindo@gmail.com>
2020-05-27 07:58:04 -0300

    criado as pastas index.html / js/main.js / css.estilo.css

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+/* Inicio Fazendo Olá mundo aparecer na pagina web - estando em h1. */
+
+var primeiraFrase= "Olá Mundo" 
+
+document.write("<h1>" + primeiraFrase +"</h1>");
+
+/* Final  */
+
+/* Inicio Fazendo uma Array  */
+
+var primeiroArray = new Array ("HTML" + 5 + " " + "CSS" + 3 + " "  + "JAVASCRIPT" );
+
+document.write(primeiroArray);
